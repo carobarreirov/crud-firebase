86e6a6d94362d8ffb7bf94c9311425648565dde9
EduardoGueva <edu.gl@comunidad.unam.mx>
2021-10-12 20:26:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,10 @@ window.addEventListener('DOMContentLoaded', async (e) => {
 
             const btnsDelete = document.querySelectorAll('.btn-delete');
             btnsDelete.forEach(btn => {
-                btn.addEventListener('click', async (e) => await deleteTask(e.target.db.id))
+                btn.addEventListener('click', async (e) => {
+                    console.log(e.target.db.id)
+                    await deleteTask(e.target.db.id)
+                })
             })
         });
     })  
